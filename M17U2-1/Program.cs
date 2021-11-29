4eaf035b6879ec8e5e633aebb34618ee11022754
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-11-29 23:28:50 +0600

    Добавьте файлы проекта.

---
+++ M17U2-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace M17U2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton Connection { get; set; }
+        private Singleton() { }
+
+        public static Singleton ConnectTo(string configuration)
+        {
+            if (Connection == null)
+                Connection = new Singleton();
+            return Connection;
+        }
+    }
+}
---
+++ M17U2-2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace M17U2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Monster
+    {
+        private IMovement movement;
+        private IWeapon weapon;
+
+    }
+
+    interface IMonsterFactory
+    {
+        IWeapon CreateWeapon();
+        IMovement CreateMovement();
+    }
+
+    class DragonFactory : IMonsterFactory
+    {
+        public IMovement CreateMovement()
+        {
+            return new FlyMovement();
+        }
+
+        public IWeapon CreateWeapon()
+        {
+            return new FireBreath();
+        }
+    }
+
+    class OrcFactory : IMonsterFactory
+    {
+        public IMovement CreateMovement()
+        {
+            return new RunMovement();
+        }
+
+        public IWeapon CreateWeapon()
+        {
+            return new Axe();
+        }
+    }
+
+    interface IWeapon
+    {
+        void Attack();
+    }
+    class FireBreath : IWeapon
+    {
+        public void Attack()
+        {
+            Console.WriteLine("ОГОНЬ...🎇");
+        }
+    }
+
+    class Axe : IWeapon
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Топорик...");
+        }
+    }
+
+    interface IMovement
+    {
+        void StartMoving();
+    }
+
+    class FlyMovement : IMovement
+    {
+        public void StartMoving()
+        {
+            Console.WriteLine("Летим...");
+        }
+    }
+
+    class RunMovement : IMovement
+    {
+        public void StartMoving()
+        {
+            Console.WriteLine("БЕЖИМ");
+        }
+    }
+}
